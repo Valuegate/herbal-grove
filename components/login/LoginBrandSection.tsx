158d5c0a0269d833ad097c0b09f2b7c53b97c1d9
Emmanuel <henry12cage@gmail.com>
2026-05-22 09:30:45 +0100

    Forgot password & Home button UI

--- components/login/LoginBrandSection.tsx
+++ components/login/LoginBrandSection.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
-<<<<<<< HEAD
-import Padlock from "@/components/signup/morter.png";
-=======
 import Morter from "@/components/signup/morter.png";
->>>>>>> 8ebdb00697fe3b15c10ede1990286a97040dcccd
 
 function LeafLogo() {
   return (
@@ -38,45 +34,26 @@ function LeafLogo() {
 
 export const BrandSection = () => {
   return (
-<<<<<<< HEAD
     <div className="p-5 flex flex-col items-center justify-center h-full text-center">
-=======
-    <div className="p-8 flex flex-col items-center justify-center h-full text-center">
->>>>>>> 8ebdb00697fe3b15c10ede1990286a97040dcccd
       <div className="mb-8 flex items-center space-x-2">
         <LeafLogo />
         <span className="font-bold text-2xl text-[#1a7a1e]">HerbaGrove</span>
       </div>
 
-<<<<<<< HEAD
       <div className="mb-2">
         <h2 className="text-3xl font-extrabold text-black mb-3">
           Welcome Back!
         </h2>
         <p className="text-gray-600 max-w-[280px] mx-auto leading-relaxed">
-=======
-      <div className="mb-8">
-        <h2 className="text-3xl font-extrabold text-black mb-3">
-          Welcome Back!
-        </h2>
-        <p className="text-gray-600 max-w-[280px] mx-auto leading-relaxed italic">
->>>>>>> 8ebdb00697fe3b15c10ede1990286a97040dcccd
           "Nature's wisdom, AI's Precision. Discover the power of botanical
           healing personalized for your unique needs."
         </p>
       </div>
 
-<<<<<<< HEAD
-      <div className=" mb-3 flex justify-center w-full max-w-[220px]">
-        <Image
-          src={Padlock}
-          alt="Locked Heart"
-=======
       <div className="mt-3 mb-3 flex justify-center w-full max-w-[220px]">
         <Image
           src={Morter}
           alt="Mortar and Pestle"
->>>>>>> 8ebdb00697fe3b15c10ede1990286a97040dcccd
           className="w-full h-auto object-contain drop-shadow-2xl"
         />
       </div>
